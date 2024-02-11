4e0aca79496a3f2ac9326ca49f85ad687089b386
moonblade <moonblade168@gmail.com>
2024-02-11 21:42:21 +0530

    remove cache

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from src.logger import get_logger
 from src.gdrive import list_files
+from src.utils import cleanFileName
 
 
 logger = get_logger()
 def main():
     gdriveFolder = "1myOC0YxE5rjbCI-vwe0ZcskNJ-9Tb6IL"
     files = list_files(gdriveFolder)
-    logger.debug(files)
+    for file in files:
+        logger.debug(f"Checking {file['name']}")
+        fileName = cleanFileName(file["name"])
+        exit()
 
 if __name__ == "__main__":
     main()
